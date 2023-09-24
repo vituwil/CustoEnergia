5b7feb32ed78a994773b0f3321947139d91223ac
Victor <victorrefundine@hotmail.com>
2023-09-24 20:05:11 -0300

    Adição do programa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double consumo, horasPorDia;
+decimal custo, estimativaPorMes;
+
+Console.WriteLine(" Custo de Energia \n");
+
+Console.WriteLine("Consumo do aparelho (em kWh/mês):");
+consumo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Horas de uso por dia:");
+horasPorDia = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Custo da energia (emR$/kWh):");
+custo = Convert.ToDecimal(Console.ReadLine());
+
+estimativaPorMes = Convert.ToDecimal(consumo *horasPorDia) * custo;
+
+Console.WriteLine($"\nCusto estimado: {estimativaPorMes:C}");
